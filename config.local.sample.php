915baca92e8c6a7b9cd16b6b5849117a7271e75c
David Lane <david.lane@logistikgroup.com>
2013-07-18 12:57:29 +0100

    Adding in moved wp-content folder, plus the new config file to accomodate it

---
+++ config.local.sample.php
@@ -0,0 +1,8 @@
+// Make a copy of this file and put in your local database connection details
+// Rename it to config.local.php
+<?php
+  define('DB_NAME', '');
+  define('DB_USER', '');
+  define('DB_PASSWORD', '');
+  define('WP_DEBUG', true);	
+
