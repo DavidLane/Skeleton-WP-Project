c3122a3784a42ebf7b678fce66cbce67c4cfc8d0
David Lane <david.lane@logistikgroup.com>
2013-07-18 13:24:58 +0100

    updating config sample

--- config.local.sample.php
+++ config.local.sample.php
@@ -1,6 +1,6 @@
-// Make a copy of this file and put in your local database connection details
-// Rename it to config.local.php
 <?php
+	// Make a copy of this file and put in your local database connection details
+	// Rename it to config.local.php
   define('DB_NAME', '');
   define('DB_USER', '');
   define('DB_PASSWORD', '');
